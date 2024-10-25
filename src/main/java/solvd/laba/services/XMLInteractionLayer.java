e7946a388504f75d5afc43acb1ad5fba39e6472f
vlzJuan <valencellojuan@gmail.com>
2024-10-25 12:36:47 -0300

    [ServiceLayer]: - Added the ```createObjectForDAO``` and ```parseInput``` methods, originally from DBInteractionLayer. [XMLInteractionLayer]: - Implemented the menu functionalities. Now the only thing left is to ensure all daos work correctly. [XMLValidator]: Created the helper function that allos the validation of an XML file given a XSD schema.

--- src/main/java/solvd/laba/services/XMLInteractionLayer.java
+++ src/main/java/solvd/laba/services/XMLInteractionLayer.java
@@ -2,13 +2,19 @@ package solvd.laba.services;
 
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
-import solvd.laba.xml.XMLValidator;
+import solvd.laba.dao.AbstractDAO;
+import solvd.laba.xml.XMLAbstractDAO;
+import solvd.laba.xml.daos.*;
+import solvd.laba.xml.utilities.XMLValidator;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -17,6 +23,19 @@ public class XMLInteractionLayer {
     private static String xmlPath = "src/main/resources/university.xml";
     private static String xsdPath = "src/main/resources/university.xsd";
     private static Document document;
+    private static ArrayList<Object> daoInstances;
+
+
+    public XMLInteractionLayer(){
+        daoInstances = new ArrayList<>();
+        daoInstances.add(new XMLStudentDAO(document));
+        daoInstances.add(new XMLProfessorDAO(document));
+        daoInstances.add(new XMLOfficeDAO(document));
+        daoInstances.add(new XMLDepartmentDAO(document));
+        daoInstances.add(new XMLCareerDAO(document));
+    }
+
+
 
     public static boolean validate(Scanner scan){
 
@@ -34,11 +53,11 @@ public class XMLInteractionLayer {
         return XMLValidator.validate(xmlPath, xsdPath);
     }
 
-    public static void execute(Scanner scan){
+    public void execute(Scanner scan){
         if(loadDocument()){
             xmlMainMenu(scan);
         }
-
+        //Here, save the document.
     }
 
     private static boolean loadDocument() {
@@ -57,16 +76,60 @@ public class XMLInteractionLayer {
         return ret;
     }
 
+    //private static void saveDocument();
 
-    private static void xmlMainMenu(Scanner scan){
+    private void xmlMainMenu(Scanner scan){
+
+        boolean processMenu = true;
+        while (processMenu) {
+            System.out.println("Select a DAO and method to execute (or '-1' to exit):");
+
+            int index = 0;
+            for (Object daoInstance : daoInstances) {
+                System.out.println("DAO " + index + ": " + daoInstance.getClass().getSimpleName());
+                index++;
+            }
+
+            System.out.print("Enter DAO number: ");
+            try {
+                int daoNum = scan.nextInt();
+
+                if(daoNum==-1){
+                    processMenu = false;
+                    System.out.println("XML menu exited. Have a good day!");
+                }
+                else if (daoNum < -1 || daoNum >= daoInstances.size()) {
+                    System.out.println("Invalid selection. Please try again.");
+                }
+                else{
+                    processMenu = false;
+                    if(daoInstances.get(daoNum) instanceof XMLAbstractDAO<?,?> dao){
+                        this.xmlOperationMenu(scan, dao);
+                    }
+                    else{
+                        System.out.println("Double-keyed DAO implementation TBI");
+                    }
+                }
+            } catch (InputMismatchException exc) {
+                System.out.println("Please enter an integer.");
+            }
+        }
+
+    }
+
+
+    @SuppressWarnings("unchecked")
+    private <T, ID> void xmlOperationMenu(Scanner scan, XMLAbstractDAO<T,ID> daoInstance){
 
         boolean menuLoop = true;
         int option;
         while(menuLoop){
             System.out.println("Select an option:");
             System.out.println("1 - Add an entry");
-            System.out.println("2 - Modify an entry");
-            System.out.println("3 - Remove an entry");
+            System.out.println("2 - Read an entry");
+            System.out.println("3 - Modify an entry");
+            System.out.println("4 - Delete an entry");
+            System.out.println("5 - View all entries");
             System.out.println("-1 - Exit the program.");
 
             try{
@@ -75,13 +138,25 @@ public class XMLInteractionLayer {
 
                 switch(option){
                     case 1:
-                        System.out.println("Add entry menu, TBI");
+                        daoInstance.insert((T) ServiceLayer.createObjectForDAO(scan, daoInstance));
                         break;
                     case 2:
-                        System.out.println("Modify entry menu, TBI");
+                        System.out.println("Enter the key required to read: ");
+                        System.out.println(daoInstance.read((ID)scan.nextLine()).toString());
                         break;
                     case 3:
+                        daoInstance.update((T) ServiceLayer.createObjectForDAO(scan, daoInstance));
                         System.out.println("Remove entry menu, TBI");
+                        break;
+                    case 4:
+                        System.out.println("Enter the key required to delete: ");
+                        System.out.println(daoInstance.read((ID)scan.nextLine()).toString());
+                        break;
+                    case 5:
+                        System.out.println("The complete result set is:");
+                        for (T value: daoInstance.findAll()) {
+                            System.out.println(value);
+                        }
                         break;
                     case -1:
                         menuLoop = false;
