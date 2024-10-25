ff42c8f7223adbdfa50e36f767109cd23b41a9fb
vlzJuan <valencellojuan@gmail.com>
2024-10-25 01:18:42 -0300

    Modified the ```retry``` parameter from int to Integer in the class and the constructor, so that it allows for null values entries

--- src/main/java/solvd/laba/tableclasses/ExamScore.java
+++ src/main/java/solvd/laba/tableclasses/ExamScore.java
@@ -5,17 +5,12 @@ public class ExamScore {
     public int examId;
     public int enrollmentId;
     public int score;
-    public int retryScore;
+    public Integer retryScore;
 
-
-    public ExamScore(int examId, int enrollmentId, int score){
+    public ExamScore(int examId, int enrollmentId, int score, Integer retryScore){
         this.examId = examId;
         this.enrollmentId = enrollmentId;
         this.score = score;
-    }
-
-    public ExamScore(int examId, int enrollmentId, int score, int retryScore){
-        this(examId, enrollmentId, score);
         this.retryScore = retryScore;
     }
 
