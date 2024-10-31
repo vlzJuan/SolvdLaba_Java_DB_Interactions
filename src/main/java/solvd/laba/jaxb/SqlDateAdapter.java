8ab17cdb0015b2eaa855ed1b772c51bcb6db0462
vlzJuan <valencellojuan@gmail.com>
2024-10-31 10:44:27 -0300

    Created [SqlDateAdapter] extending [XmlAdapter] from the Jaxb dependency imported in maven. It will be used to marshall and unmarshall java.util.sql.Date types from the table classes

---
+++ src/main/java/solvd/laba/jaxb/SqlDateAdapter.java
@@ -0,0 +1,17 @@
+package solvd.laba.jaxb;
+
+import jakarta.xml.bind.annotation.adapters.XmlAdapter;
+import java.sql.Date;
+
+public class SqlDateAdapter extends XmlAdapter<String, Date> {
+
+    @Override
+    public Date unmarshal(String v) {
+        return Date.valueOf(v); // Parses String to java.sql.Date
+    }
+
+    @Override
+    public String marshal(Date v) {
+        return v.toString(); // Converts java.sql.Date to String
+    }
+}
