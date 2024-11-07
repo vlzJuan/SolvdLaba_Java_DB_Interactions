a016ece139522c31b52c4c72970f9cbc877aa024
vlzJuan <valencellojuan@gmail.com>
2024-11-06 21:07:00 -0300

    - Modified display of interfaces so it takes the proper name of the interface - Added method getEntityClass to try and create the objects for the insert and update statements for the mappers - ADDED COMMENT ON THE ISSUE for insert and update statements

--- src/main/java/solvd/laba/services/MyBatisSqlLayer.java
+++ src/main/java/solvd/laba/services/MyBatisSqlLayer.java
@@ -9,6 +9,7 @@ import solvd.laba.mybatis.interfaces.StudentMapper;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -40,8 +41,8 @@ public class MyBatisSqlLayer {
             System.out.println("Select a DAO and method to execute (or '-1' to exit):");
 
             int index = 0;
-            for (Object daoInstance : mapperInterfaces) {
-                System.out.println("DAO " + index + ": " + daoInstance.getClass().getSimpleName());
+            for (Class<?> daoInterface : mapperInterfaces) {
+                System.out.println("DAO " + index + ": " + daoInterface.getSimpleName());
                 index++;
             }
 
@@ -67,7 +68,6 @@ public class MyBatisSqlLayer {
     }
 
 
-
     @SuppressWarnings("unchecked")
     public <T, ID> void currentMapper(Scanner scanner, Class<? extends CoreDAO<T, ID>> mapperClass) {
         boolean processMenu = true;
@@ -83,7 +83,9 @@ public class MyBatisSqlLayer {
 
             try (SqlSession session = sqlSessionFactory.openSession()) {
                 CoreDAO<T, ID> mapper = session.getMapper(mapperClass);
-                System.out.println("DEBUG: Mapper = "+ mapper.toString());
+
+                // Retrieve the actual class of T at runtime
+                Class<T> entityClass = getEntityClass(mapperClass);
 
 
                 int option = scanner.nextInt();
@@ -91,12 +93,18 @@ public class MyBatisSqlLayer {
                 ID id;
                 String aux;
 
-
-
                 switch (option) {
                     case 1:
-                        T newEntity = (T)ServiceLayer.createObjectForClass(scanner, sdasd);
-
+                        //  This case nor the update properly creates the auxiliary T-type through Reflection.
+                        //  The issue likely stems from the implementation of createObjectFromClass,
+                        //  which is an offshoot of the ServiceClass.createObjectForDao method
+                        //  to create an entity through reflection.
+                        //  The method returns null pretty much always, and likely has to do with the fact
+                        //  that, by trying to take the generic T-type from an interface (instead of
+                        //  an instance of an object, as it was in the ServiceClass referenced method),
+                        //  the type is not properly captures and no input prompts are displayed.
+                        T newEntity = (T) ServiceLayer.createObjectForClass(scanner, entityClass);
+                        System.out.println(newEntity.toString());
                         mapper.insert(newEntity);
                         session.commit();
                         break;
@@ -105,14 +113,14 @@ public class MyBatisSqlLayer {
                         aux = scanner.nextLine();
                         id = (ID) ServiceLayer.parseInput(aux, Integer.class);
                         T result = mapper.read(id);
-                        if(result!=null){
+                        if (result != null) {
                             System.out.println("The read entry is: \n" + result);
-                        }
-                        else{
+                        } else {
                             System.out.println("No entry found with that ID");
                         }
                         break;
                     case 3:
+                        //  Has issues too
                         T updatedEntity = (T) ServiceLayer.createObjectForClass(scanner, entityClass);
                         mapper.update(updatedEntity);
                         session.commit();
@@ -124,8 +132,7 @@ public class MyBatisSqlLayer {
                         try {
                             mapper.delete(id);
                             session.commit();
-                        }
-                        catch (Exception exc){
+                        } catch (Exception exc) {
                             System.out.println("Could not commit deletion. The selected row has a constraint attached.");
                         }
                         break;
@@ -134,20 +141,41 @@ public class MyBatisSqlLayer {
                         System.out.println("The complete result set is:");
                         resultList.forEach(System.out::println);
                         break;
-                    case 6:
-                        System.out.println("TBI.");
-                        break;
                     case -1:
                         processMenu = false;
                         System.out.println("MyBatis Interaction menu exited. Have a good day!");
                         break;
-                    default: System.out.println("Invalid selection. Please try again.");
+                    default:
+                        System.out.println("Invalid selection. Please try again.");
                         break;
                 }
             } catch (InputMismatchException exc) {
                 System.out.println("Please enter an integer.");
+                scanner.nextLine(); // Clear invalid input's newline
+            } catch (Exception e) {
+                System.out.println("An error occurred: " + e.getMessage());
             }
         }
+    }
+
+    private static <T, ID> Class<T> getEntityClass(Class<? extends CoreDAO<T, ID>> mapperClass) {
+        Class<T> entityClass = null;
+        Type[] interfaces = mapperClass.getGenericInterfaces();
+
+        for (Type interfaceType : interfaces) {
+            if (interfaceType instanceof ParameterizedType) {
+                ParameterizedType paramType = (ParameterizedType) interfaceType;
+                if (paramType.getRawType().equals(CoreDAO.class)) {
+                    entityClass = (Class<T>) paramType.getActualTypeArguments()[0];
+                    break;
+                }
+            }
+        }
+
+        if (entityClass == null) {
+            throw new IllegalStateException("Could not determine entity class type.");
+        }
+        return entityClass;
     }
 
 
