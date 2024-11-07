481f59033e439999019aa2844f3219a24be2bc19
vlzJuan <valencellojuan@gmail.com>
2024-11-06 21:30:11 -0300

    - Added @Mapper tag to interface. Removed explicit overridden method declaration from body

--- src/main/java/solvd/laba/mybatis/interfaces/StudentMapper.java
+++ src/main/java/solvd/laba/mybatis/interfaces/StudentMapper.java
@@ -1,17 +1,14 @@
 package solvd.laba.mybatis.interfaces;
 
+import org.apache.ibatis.annotations.Mapper;
 import solvd.laba.dao.CoreDAO;
 import solvd.laba.tableclasses.Student;
-
 import java.util.List;
 
 /**
  * Interface for MyBatis
  */
+@Mapper
 public interface StudentMapper extends CoreDAO<Student, Integer> {
-
-    List<Student> getStudentByName(String name);
-    List<Student> getStudentBySurname(String surname);
-    List<Student> getStudentByCareerId(Integer careerId);
-
+    // No need to explicitly override methods here
 }
